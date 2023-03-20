634790357d69fe175fe2c3342bfcb48ae94811b6
bc_01 <46893535+kimbc814@users.noreply.github.com>
2023-03-20 21:53:59 +0900

    Add files via upload

---
+++ 2023/03/20/GUI_PROJECT/IO_control.c
@@ -0,0 +1,18 @@
+#include "IfxPort.h"
+#define a1     &MODULE_P00,0
+#define a2     &MODULE_P00,1
+#define a3     &MODULE_P00,2
+#define a4     &MODULE_P00,3
+#define a5     &MODULE_P00,8
+#define a6     &MODULE_P00,9
+#define a7     &MODULE_P00,10
+#define a8     &MODULE_P00,11
+
+#define b1     &MODULE_P22,0
+#define b2     &MODULE_P22,1
+#define b3     &MODULE_P22,2
+#define b4     &MODULE_P21,0
+#define b5     &MODULE_P22,3
+#define b6     &MODULE_P21,4
+#define b7     &MODULE_P21,5
+#define b8     &MODULE_P20,10
